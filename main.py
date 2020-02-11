5c3150b2a4e8a88434d99a77c29d260186c48b78
Evan Canfield <canfielder@gmail.com>
2020-02-11 18:12:29 +0000

    Added port to main.py

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ def predict():
 
 if __name__ == '__main__':
     clf = joblib.load('wine_predict/wine_quality_prediction.joblib')
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
