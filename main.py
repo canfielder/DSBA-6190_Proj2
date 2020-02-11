3d524f8633ea8a7095468d952b013da44cf1247b
Evan Canfield <canfielder@gmail.com>
2020-02-11 01:42:43 +0000

    Update main.py to include sklearn model

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+
+import pandas as pd
+from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
 
+# Functions
+def scale(payload):
+    """Scales Payload"""
+
+    LOG.info(f"Scaling Payload: {payload}")
+    scaler = StandardScaler().fit(payload)
+    scaled_adhoc_predict = scaler.transform(payload)
+    return scaled_adhoc_predict
+
+# App Pages
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    html = f"<h2>Sklearn Wine Quality Prediction Home</h2>"
+    return html.format(format)
+
+@app.route("/predict", methods=['POST'])
+def predict():
+        """Performs an sklearn prediction
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+    result looks like:
+    { "prediction": [ 20.35373177134412 ] }
+    """
+    
+    json_payload = request.json
+    LOG.info(f"JSON payload: {json_payload}")
+    
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info(f"inference payload DataFrame: {inference_payload}")
+    
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
+    
+    return jsonify({'prediction': prediction})
 
 
 if __name__ == '__main__':
+    clf = joblib.load('wine_quality_prediction.joblib')
     app.run(debug=True, host='0.0.0.0')
